e175f41518ee4b6cd2cbdb959b378c55464ef56a
eduardpafe08 <eduardpafe@gmail.com>
2025-08-21 19:56:16 +0200

    .env i git ignore

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,18 @@
 import mysql from 'mysql2/promise'
+import dotenv from 'dotenv'
+dotenv.config()
+const pe = process.env
+
+
+export const API_PORT = pe.API_PORT
+export const HOST = pe.DB_HOST
+const USER = pe.DB_USER
+const PASSWORD = pe.DB_PASSWORD
+const DATABASE = pe.DB_DATABASE
 
 export const pool = mysql.createPool({
-    host: '192.168.1.240',
-    user: 'todolist_db',
-    password: 'Patata123!Todolist_db',
-    database: 'todolist_db'
+    host: HOST,
+    user: USER,
+    password: PASSWORD,
+    database: DATABASE
 })
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 import router from './routes.js'
+import { HOST, API_PORT } from './db.js'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/api', router)
 
-app.listen(3000, () => {
-    console.log(`Servidor corriendo en http://localhost:3000`)
+app.listen(API_PORT, () => {
+    console.log(`Servidor corriendo en http://${HOST}:${API_PORT}`)
 })
